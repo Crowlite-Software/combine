60504901c0d837708010af5c5c481412d0333ac6
Erin <28122025+AWildErin@users.noreply.github.com>
2021-05-17 22:03:02 +0100

    Add summary to `GetAllItems()`

--- code/Util/ItemUtils.cs
+++ code/Util/ItemUtils.cs
@@ -10,10 +10,11 @@ namespace CombineControl.Util
 	public static class ItemUtils
 	{
 		/// <summary>
-		/// 
+		/// Lists all the items in CombineControl or, if specified, lists all the items
+		/// matching the filter.
 		/// </summary>
-		/// <param name="filter"></param>
-		/// <returns></returns>
+		/// <param name="filter">Optional search filter</param>
+		/// <returns>A list of the items within the filter</returns>
 		public static IEnumerable<BaseItem> GetAllItems(string filter = "")
 		{
 			return from item in ItemSystem.Items where item.ItemName.Contains( filter, StringComparison.OrdinalIgnoreCase ) select item;
