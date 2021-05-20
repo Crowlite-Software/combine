4d8f4666e5c8243b6c65d9b68960093ee1f94e3b
Erin <28122025+AWildErin@users.noreply.github.com>
2021-05-20 19:04:35 +0100

    Add `rp_dev_damageme`

---
+++ code/Util/PlayerUtil.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+
+namespace CombineControl.Util
+{
+	public static class PlayerUtil
+	{
+		///
+		/// Debug Console Commands
+		///
+
+		/// <summary>
+		/// Damages the player with a specified amount of damage
+		/// From: https://github.com/ampersoftware/Team-Fortress-Source-2/blob/master/code/player/CTFPlayer.cs#L77-L84
+		/// </summary>
+		[ServerCmd( "rp_dev_damageme" )]
+		public static void DamageMeCmd( int damage )
+		{
+			if ( ConsoleSystem.Caller.Pawn is CCPlayer player )
+			{
+				int oldHealth = player.Health;
+				player.TakeDamage( DamageInfo.FromBullet( player.Position, Vector3.Zero, damage ) );
+
+				Log.Info( $"Old health: {oldHealth}, new health: {player.Health}" );
+			}
+		}
+	}
+}
