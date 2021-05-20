b344db6b0b05004948cba8c6065d3b7616d858ed
Erin <28122025+AWildErin@users.noreply.github.com>
2021-05-20 17:41:46 +0100

    Load all systems when we construct the game

--- code/CCGame.cs
+++ code/CCGame.cs
@@ -1,5 +1,8 @@
+using CombineControl.Systems;
 using CombineControl.UI;
 using Sandbox;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CombineControl
 {
@@ -11,6 +14,22 @@ namespace CombineControl
 			if ( IsServer )
 			{
 				new CCHud();
+			}
+
+			// Initialise all of our systems
+			// I'm not sure if this is the best method, but it's good enough for now.
+			var allSystems = from system in Library.GetAll<BaseSystem>()
+							 where !system.IsAbstract
+							 select system;
+			foreach ( var system in allSystems )
+			{
+				var systemClass = Library.Create<BaseSystem>( system.FullName );
+				Log.Info( $"Found and initialising: {system.FullName}" );
+
+				if ( systemClass.Init() )
+				{
+					BaseSystem.SystemsList.Add( systemClass );
+				}
 			}
 		}
 
---
+++ code/Systems/BaseSystem.cs
@@ -0,0 +1,19 @@
+using Sandbox;
+using System.Collections.Generic;
+
+namespace CombineControl.Systems
+{
+	/// <summary>
+	/// This is the base class for all our systems.
+	/// All base systems are intitliased when the game class is constructed.
+	/// </summary>
+	public abstract class BaseSystem : NetworkClass
+	{
+		public static List<BaseSystem> SystemsList = new List<BaseSystem>();
+
+		/// <summary>
+		/// Called when the game is constructed
+		/// </summary>
+		public abstract bool Init();
+	}
+}
